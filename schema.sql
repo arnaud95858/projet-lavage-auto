cdc43451d9ec6bf575b85138728c81421ded138b
arnaud95858 <nahumarnaud407@gmail.com>
2025-07-10 20:36:11 +0200

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Maquette Figma</title>
+</head>
+<body>
+  <h1>Table depuis Figma</h1>
+  <iframe
+    style="border: none;"
+    width="800"
+    height="600"
+    src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/TON-LIEN"
+    allowfullscreen>
+  </iframe>
+</body>
+</html>
